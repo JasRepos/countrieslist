2444a5ad2f97fb2ed79426d66f0ca8226996c121
Jaswanth Kumar Chigurupati <jas.codemine@example.com>
2020-07-22 13:10:06 +0100

    Updates

---
+++ precache-manifest.a1beb017153202d0056182b39841705e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1787b995d87cabdbac3b5a0aa6ba72",
+    "url": "/countrieslist/index.html"
+  },
+  {
+    "revision": "abd43024e4babdab0e50",
+    "url": "/countrieslist/static/css/main.83a943b3.chunk.css"
+  },
+  {
+    "revision": "fe96a296751cf2182828",
+    "url": "/countrieslist/static/js/2.badeefc0.chunk.js"
+  },
+  {
+    "revision": "174263e5b9d3e85c08d4329eaeeb7640",
+    "url": "/countrieslist/static/js/2.badeefc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd43024e4babdab0e50",
+    "url": "/countrieslist/static/js/main.2d9f76dc.chunk.js"
+  },
+  {
+    "revision": "2d533f551717fcb332ba",
+    "url": "/countrieslist/static/js/runtime-main.fa81596d.js"
+  }
+]);
